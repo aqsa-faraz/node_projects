c93542e4432f3c4111cee9d39b1affa21207ab22
aqsa-faraz <150932703+aqsa-faraz@users.noreply.github.com>
2023-12-27 22:35:00 +0500

    first commit

---
+++ calculator/main.js
@@ -0,0 +1,35 @@
+// calculator using inquirer
+import inquirer from 'inquirer';
+let answer = await inquirer.prompt([{
+        type: 'number',
+        name: 'operand1',
+        message: 'Enter first number?'
+    },
+    {
+        type: 'number',
+        name: 'operand2',
+        message: 'Enter second number?'
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: 'Choose your operation?',
+        choices: ['addition', 'subtraction', 'multiplication', 'division']
+    }]);
+if (answer.operator === 'addition') {
+    console.log(`${answer.operand1} + ${answer.operand2} = ${answer.operand1 + answer.operand2}`);
+}
+else if (answer.operator === 'subtraction') {
+    console.log(`${answer.operand1} - ${answer.operand2} = ${answer.operand1 - answer.operand2}`);
+}
+else if (answer.operator === 'multiplication') {
+    console.log(`${answer.operand1} * ${answer.operand2} = ${answer.operand1 * answer.operand2}`);
+}
+else {
+    if (answer.operand2 != 0) {
+        console.log(`${answer.operand1} / ${answer.operand2} = ${answer.operand1 / answer.operand2}`);
+    }
+    else {
+        console.log("Can not divide by 0");
+    }
+}
